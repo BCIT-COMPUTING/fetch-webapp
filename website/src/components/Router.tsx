be57c2d6e5541b1a0a8c5211b3ff1ae434e42fc6
Jason Rivera <58869791+jason-rivera@users.noreply.github.com>
2022-03-08 00:44:36 -0800

    Created registration page

--- website/src/components/Router.tsx
+++ website/src/components/Router.tsx
@@ -2,6 +2,7 @@
 import { AppContext, useAppContext } from "../store/appContext";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import TestPage from "./Test/TestPage";
+import SignupPage from "./Signup/SignupPage";
 
 function AppRouter() {
   const appContext = useAppContext();
@@ -11,6 +12,7 @@ function AppRouter() {
       <BrowserRouter>
         <Routes>
           <Route path="/test" element={<TestPage />} />
+          <Route path="/signup" element={<SignupPage />} />
         </Routes>
       </BrowserRouter >
     </AppContext.Provider>
