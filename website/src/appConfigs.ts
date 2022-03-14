0f7c700af45e83d8a90bf7a425b0a34845a11d1e
Divanshu <sharma.divanshu30@gmail.com>
2022-03-14 01:40:42 -0700

    frontend refactor- register page

--- website/src/appConfigs.ts
+++ website/src/appConfigs.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMmVkZmExYzhkOWVmMGUyYmM1NDdjOSIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE2NDcyMzk3MzIsImV4cCI6MTY0NzQ5ODkzMn0.nyMWuCAfO9pQRggRLsckXlgrG6tCrRC9a5ScwOKCjr0";
 // export const endPointBaseUrl = "https://fetch-be.azurewebsites.net";
-export const endPointBaseUrl = "http://localhost:8080/api/v1/";
+export const endPointBaseUrl = "http://localhost:8080/api/v1";
 
 export const publicRequest = axios.create({
   baseURL: endPointBaseUrl,
--- website/src/components/Signup/SignupPage.tsx
+++ website/src/components/Signup/SignupPage.tsx
@@ -1,31 +1,19 @@
 import { useAppStore } from "../../store/appContext";
 import { useState } from "react";
 import styles from "./SignupPage.module.css";
-import axios from "axios";
-import * as crypto from "crypto-js";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
-import { endPointBaseUrl } from "../../appConfigs";
+import { endPointBaseUrl, publicRequest } from "../../appConfigs";
 
 const SignupPage = () => {
   const navigate = useNavigate();
 
   const { state, setState } = useAppStore();
-  // const { isLoggedIn } = state;
-
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [dogName, setDogName] = useState("");
-  const [dogAge, setDogAge] = useState("");
-  const [dogGender, setDogGender] = useState("");
-  const [dogUrl, setDogUrl] = useState("");
-
-  const encryptPassword = (pw: string): void => {
-    setPassword(crypto.SHA256(pw).toString());
-  };
 
   const isValid = (username: string, password: string): boolean => {
     if (!username) {
@@ -48,22 +36,20 @@ const SignupPage = () => {
       return;
     }
 
-    const response = await axios
-      .post(endPointBaseUrl + "/signup", {
-        firstName: firstName,
-        lastName: lastName,
+    const res = await publicRequest
+      .post(endPointBaseUrl + "/register", {
+        firstname: firstName,
+        lastname: lastName,
         username: username,
         email: email,
         password: password,
-        dogName: dogName,
-        dogAge: dogAge,
-        dogGender: dogGender,
-        dogUrl: dogUrl,
       })
       .then((response) => {
         if (response.status == 200) {
+          console.log(response.data);
+          setState({ isLoggedIn: true, user: response.data });
           toast.success("Signup successful");
-          navigate("/login");
+          navigate("/dogInfo");
         }
       })
       .catch((error) => {
@@ -125,57 +111,17 @@ const SignupPage = () => {
                 type="password"
                 placeholder="Enter your password"
                 name="password"
-                onChange={(event) => encryptPassword(event.target.value)}
+                onChange={(event) => setPassword(event.target.value)}
               />
             </div>
-            <div className={styles.labelSection}>
-              <div className={styles.signupLabel}>Dog Name: </div>
-              <input
-                id="dog-name-input"
-                type="text"
-                placeholder="Enter your dog's name"
-                name="dogName"
-                onChange={(event) => setDogName(event.target.value)}
-              />
-            </div>
-            <div className={styles.labelSection}>
-              <div className={styles.signupLabel}>Dog Age: </div>
-              <input
-                id="dog-age-input"
-                type="text"
-                placeholder="Enter your dog's age"
-                name="dogAge"
-                onChange={(event) => setDogAge(event.target.value)}
-              />
-            </div>
-            <div className={styles.labelSection}>
-              <div className={styles.signupLabel}>Dog Gender: </div>
-              <select
-                name="dogGender"
-                id="dog-gender-input"
-                onChange={(event) => setDogGender(event.target.value)}
-              >
-                <option defaultValue=""></option>
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-              </select>
-            </div>
-            <div className={styles.labelSection}>
-              <div className={styles.signupLabel}>Dog Picture (URL): </div>
-              <input
-                id="dog-picture-input"
-                type="text"
-                placeholder="http://imgur...etc"
-                name="dogPicture"
-                onChange={(event) => setDogUrl(event.target.value)}
-              />
-            </div>
-            <input
+            <button
               className={styles.signupBtn}
               type="button"
               value="Signup"
-              onClick={() => signup()}
-            />
+              onClick={signup}
+            >
+              SignUp
+            </button>
           </form>
         </div>
       </div>
