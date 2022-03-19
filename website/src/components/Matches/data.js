168ff1361704f0d4c0cf12a436b0cad152046d6f
Jason Rivera <58869791+jason-rivera@users.noreply.github.com>
2022-03-19 13:25:02 -0700

    created matches page

---
+++ website/src/components/Matches/data.js
@@ -0,0 +1,31 @@
+export const MatchInfo = [
+  {
+    name: "Bronx",
+    age: 10,
+    sex: "M",
+    dateMatched: "12345",
+    image: "https://www.smalldogplace.com/images/american-eskimo-dog-250.jpg"
+  },
+  {
+    name: "Po",
+    age: 3,
+    sex: "F",
+    dateMatched: "54321",
+    image: "https://www.smalldogplace.com/images/american-eskimo-dog-250.jpg"
+  },
+  {
+    name: "Gus",
+    age: 1,
+    sex: "M",
+    dateMatched: "11111",
+    image: "https://www.smalldogplace.com/images/american-eskimo-dog-250.jpg"
+  },
+  {
+    name: "Odie",
+    age: 6,
+    sex: "M",
+    dateMatched: "22222",
+    image: "https://www.smalldogplace.com/images/american-eskimo-dog-250.jpg"
+  },
+
+]
