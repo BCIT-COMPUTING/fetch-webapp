a801e165b59afa722ec0b793567a10d8d3a489f9
Jason Rivera <58869791+jason-rivera@users.noreply.github.com>
2022-04-03 20:55:39 -0700

    swaggg

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -4,6 +4,32 @@ const admin = require('../configs/adminUtils');
 
 
 router.get("/stats", async function (req, res) {
+  /*
+    #swagger.description = 'Returns the stats for all of the endpoints',
+  } */
+
+  /* #swagger.responses[200] = {
+    description: 'Stats successfully obtained.',
+    schema: {
+      postRegister: 6,
+      postLogin: 25,
+      postVerifyJWT: 65,
+      getDogs: 34,
+      postAddDog: 77,
+      putEditDog: 23,
+      deleteDog: 6,
+      getDogByUserId: 64,
+      getDogByDogId: 23,
+      postCreateMatch: 76,
+      putAddLikeById: 87,
+      putAddViewById: 65,
+      putAddDislikeById: 37,
+      getCheckUserByUserId: 85,
+      getAllLikesByUserId: 52,
+      getStats: 65,
+    }
+  } */
+
   try {
     await admin.updateStats("getStats");
     const stats = await Stats.find();
@@ -15,6 +41,29 @@ router.get("/stats", async function (req, res) {
 });
 
 router.post("/reset", async function (req, res) {
+  /*
+  #swagger.parameters['obj'] = {
+    in: 'body',
+    description: 'Resets the stats for all of the endpoints.',
+    schema: {
+      $postRegister: 0,
+      $postLogin: 0,
+      $postVerifyJWT: 0,
+      $getDogs: 0,
+      $postAddDog: 0,
+      $putEditDog: 0,
+      $deleteDog: 0,
+      $getDogByUserId: 0,
+      $getDogByDogId: 0,
+      $postCreateMatch: 0,
+      $putAddLikeById: 0,
+      $putAddViewById: 0,
+      $putAddDislikeById: 0,
+      $getCheckUserByUserId: 0,
+      $getAllLikesByUserId: 0,
+      $getStats: 0,
+    }
+  } */
   try {
     await admin.updateStats("postReset");
     await Stats.updateOne({_id: admin.statsId }, {
